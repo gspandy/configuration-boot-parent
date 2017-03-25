2b27a1ad20acee90ac64c44dea5e06c7df90fe1d
Janita <zhuchenjian@huizhi-tech.com>
2017-03-25 11:10:13 +0800

    Initial commit

---
+++ anto-config-one/src/main/java/com/janita/autoconfig/one/AutoConfigOneApplication.java
@@ -0,0 +1,16 @@
+package com.janita.autoconfig.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017-03-25 11:07
+ */
+@SpringBootApplication
+public class AutoConfigOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConfigOneApplication.class, args);
+    }
+
+}
