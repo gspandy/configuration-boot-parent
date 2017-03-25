b23edff737e39923e24049942e9c39f815c74054
Janita <zhuchenjian@huizhi-tech.com>
2017-03-25 11:28:52 +0800

    成功

---
+++ anto-config-one/src/main/java/com/janita/autoconfig/one/controller/ConfigController.java
@@ -0,0 +1,23 @@
+package com.janita.autoconfig.one.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Janita on 2017-03-25 11:22
+ */
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public String testProfile(){
+        return environment.getProperty("profile");
+    }
+}
--- readMe.txt
+++ readMe.txt
@@ -1,2 +1,6 @@
 springboot项目中配置文件的切换
 如：开发环境使用某个配置文件，生产环境使用某个配置文件
+
+http://www.cnblogs.com/java-zhao/p/5469183.html
+
+https://my.oschina.net/u/2391658/blog/734755
